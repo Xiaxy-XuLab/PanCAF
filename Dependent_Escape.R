2bb79726d1772d2d0b621b41ffa6c2e847514220
Xiaxy-XuLab <107231387+Xiaxy-XuLab@users.noreply.github.com>
2022-06-11 22:33:28 +0800

    Add files via upload

---
+++ Dependent_Escape.R
@@ -0,0 +1,19 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Functions
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#' Hallmarker analysis
+#'
+#' This function is applied to calculate the hallmarker value for each cell
+#'
+
+RunEscape <- function(object,
+                        lib = "H") {
+    gshallmark <- getGeneSets(library = lib)
+    esseurat <- enrichIt(
+        obj = object, gene.sets = gshallmark,
+        groups = 1000, cores = 30
+    )
+    write.table(esseurat, "Hall_Marker.txt", quote = F, sep = "\t") #nolint
+    object <- Seurat::AddMetaData(object, esseurat)
+    return(object)
+}
